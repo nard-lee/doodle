4cc8049d73ce274fc8a9aa6f806a0ee1abf9f0a1
__nardLee <59244491+nard-lee@users.noreply.github.com>
2023-11-15 20:58:39 +0800

    planets2d

--- solarsystemjscanvas/sketch.js
+++ solarsystemjscanvas/sketch.js
@@ -17,22 +17,21 @@ class Planet {
         img.src = 'asset/'+this.imgsrc; 
 
         let sun = new Image();
-        sun.src = 'asset/Lava.png';
-
+        sun.src = 'asset/pl1.png';
 
         let pcx = this.x + this.r * Math.cos(this.angle);
         let pcy = this.y + this.r * Math.sin(this.angle);
 
         ctx.save();
-        ctx.translate(300, 300);
+        ctx.translate(canvas.width / 2, canvas.height / 2);
         ctx.rotate(this.rot);
         ctx.alpha = 0.4;
         strokeArc(0, 0, this.r, "#333");
         line(0, 0, pcx, pcy, "#333")
         arc(pcx, pcy, 20, "#333");
-        ctx.drawImage(img, pcx -20.5, pcy -20.5, 39, 39);
-        arc(0, 0, 21, "#333");
-        ctx.drawImage(sun, -19.4, -19.4, 39, 39);
+        ctx.drawImage(img, pcx-30, pcy-30, 60, 60);
+        arc(0, 0, 30, "#333");
+        ctx.drawImage(sun, -38, -38, 70, 70);
 
         ctx.restore();
 
@@ -44,10 +43,9 @@ class Planet {
 }
 
 let planet = [];
-let sprite = ['', 'Baren.png', 'Black_hole.png', 'Ice.png', 'Terran.png'];
 
 for(let i = 1; i <= 4; i++){
-    planet.push(new Planet(0, 0, i * 60, random(-0.03, 0.03), sprite[i]));
+    planet.push(new Planet(0, 0, i * 95, random(-0.03, 0.03),`pl${i+1}.png`));
 }
 
 
